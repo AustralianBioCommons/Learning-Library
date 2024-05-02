5821c188050a4b96c93e21ca069f37c269fec847
Patrick Capon <137233925+PatCapon39@users.noreply.github.com>
2024-05-02 16:20:39 +0930

    Rewrite table_conversion.py to match how-to hub

--- scripts/table_conversion.py
+++ scripts/table_conversion.py
@@ -1,5 +1,6 @@
 """
-Adapted from https://github.com/AustralianBioCommons/human-omics-data-sharing-field-guide/blob/main/var/tools_table_conversion.py
+Adapted from https://github.com/elixir-europe/rdmkit/blob/master/var/conversions.py
+Adapted from https://github.com/AustralianBioCommons/human-omics-data-sharing-field-guide/blob/328e84634d10b0df51e798dbf1cc114c7cb26357/var/tools_table_conversion.py
 """
 
 import pandas as pd
@@ -7,7 +8,10 @@ import yaml
 import os
 
 # --------- Variables ---------
-url = f"https://docs.google.com/spreadsheets/d/1uIBItELephFZNjC4UPoegQpo1pIzY15IjOgX8q33slU/export?format=csv&gid=0"
+google_id = "1uIBItELephFZNjC4UPoegQpo1pIzY15IjOgX8q33slU"
+gid = "0"
+url = f"https://docs.google.com/spreadsheets/d/{google_id}
+/export?format=csv&gid={gid}"
 output_path = "_data/resource_list.yml"
 rootdir = '../pages'
 allowed_registries = ['Australian BioCommons', 'EMBL-EBI', 'NCBI', 'ELIXIR Luxembourg']
@@ -19,6 +23,10 @@ resource_table = pd.read_csv(url, dtype={'name': str, 'url': str, 'related pages
 resource_list = resource_table.to_dict("records")
 clean_resource_list = []
 for resource in resource_list:
+    if not pd.isna(resource['related_pages']):
+        category_list = resource['related_pages'].rsplit(sep=",")
+    else:
+        category_list = ""
     registry_dict = {}
     for registry in allowed_registries:
         if not pd.isna(resource[registry]):
@@ -27,6 +35,8 @@ for resource in resource_list:
     clean_resource = {k: resource[k] for k in resource if not pd.isna(resource[k])}
     clean_resource_list.append(clean_resource)
     clean_resource['registry'] = registry_dict
+    if category_list != "":
+        clean_resource['related_pages'] = category_list
 
 print(os.getcwd())
 with open(output_path, 'w+') as yaml_file:
