50e64f185664c7a55e8ea17c7aeeeaf8ce78bafb
Patrick Capon <137233925+PatCapon39@users.noreply.github.com>
2024-12-02 11:12:03 +1000

    Update table_conversion.py

--- scripts/table_conversion.py
+++ scripts/table_conversion.py
@@ -7,24 +7,24 @@ import pandas as pd
 import yaml
 import os
 
-# --------- Variables - set to match your google sheet as needed ---------
+# --------- Variables - set to match the desired import google sheet as needed ---------
 google_id = "1uIBItELephFZNjC4UPoegQpo1pIzY15IjOgX8q33slU"
 gid = "0"
 url = f"https://docs.google.com/spreadsheets/d/{google_id}/export?format=csv&gid={gid}"
-output_path = "_data/tool_and_resource_list.yml"
+output_path = "_data/all_content_list.yml"
 rootdir = '../pages'
 allowed_providers = ['provider']
 
 # --------- Converting the table ---------
-
-print(f"----> Converting google table to {output_path} started.")
-resource_table = pd.read_csv(url, dtype={'name': str, 'url': str, 'description': str, 'related_pages': str, 'Topics': str, 'type': str, 'provider': str})
 # --------- Each column header in the google sheet needs to be included in the dtype= code and defined as a string (str) ---------
+print(f"----> Converting google table to {output_path} started.")
+resource_table = pd.read_csv(url, dtype={'name': str, 'url': str, 'description': str, 'collection': str, 'topics': str, 'type': str, 'provider': str})
+# --------- The collection column sets which collection the row item will be included in for the Learning Library site. The topics column allows multiple entries, must be separated by a comma and a space  ---------
 resource_list = resource_table.to_dict("records")
 clean_resource_list = []
 for resource in resource_list:
-    if not pd.isna(resource['related_pages']):
-        category_list = resource['related_pages'].rsplit(sep=", ")
+    if not pd.isna(resource['collection']):
+        category_list = resource['collection'].rsplit(sep=", ")
     else:
         category_list = ""
    # registry_dict = {}
